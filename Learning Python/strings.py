e62d463faec553fe562149451be83c33b99d9f88
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-10-26 15:29:59 +0800

    using var for strings

--- Learning Python/strings.py
+++ Learning Python/strings.py
@@ -1,1 +1,7 @@
 print("\n\nThis is how to use\n \"string")
+
+#or we can instead use variables to store strings
+
+phrase = "Genshin Impact"
+
+print(phrase + " is such a cool game") #<--this is called concatenation 
