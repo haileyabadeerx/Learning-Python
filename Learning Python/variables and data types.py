0bb196dd76b6b0f989db64040eb303999ea89701
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-10-26 15:19:53 +0800

    printing int

--- Learning Python/variables and data types.py
+++ Learning Python/variables and data types.py
@@ -5,10 +5,10 @@ age = 20 #<-- numbers
 is_girl = True #<-- booleans
 
 print("There was once a young girl named " + name + ".")
-print("She is " + age + " years old")
+print("She is " + str(age) + " years old") #<-- kailangan ganito para maprint ang numbers
 
 #can change the variable anywhere basta madeclare sya before the usage of it
 name = "Kendra"
-age = "15"
+age = 15
 print(name + " likes to play videogames")
-print("But being " + age + " makes her feel miserable")
+print("But being " + str(age) + " makes her feel miserable")
