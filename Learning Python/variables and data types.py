63e4e707e861461ff5e968ae6efb9885dd34cffb
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-10-26 15:10:21 +0800

    changing vairbles halfway

--- Learning Python/variables and data types.py
+++ Learning Python/variables and data types.py
@@ -5,5 +5,9 @@ age = "20"
 
 print("There was once a young girl named " + name + ".")
 print("She is " + age + " years old")
+
+#can change the variable anywhere basta madeclare sya before the usage of it
+name = "Kendra"
+age = "15"
 print(name + " likes to play videogames")
 print("But being " + age + " makes her feel miserable")
