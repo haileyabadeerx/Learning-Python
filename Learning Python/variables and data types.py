3505c3ee05b3873f8ca285e0b5b08cdc06e5af3d
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-10-26 15:02:41 +0800

    using variables in print

--- Learning Python/variables and data types.py
+++ Learning Python/variables and data types.py
@@ -3,7 +3,7 @@
 name = "Hailey"
 age = "20"
 
-print("There was once a young girl named Hailey")
+print("There was once a young girl named " + name + ".")
 print("Hailey is 20 years old")
 print("She likes to play videogames")
 print("But being 20 makes her feel miserable")
