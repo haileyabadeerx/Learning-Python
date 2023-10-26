a1739e18ee099a9e7bf71679ca61dbc029746423
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-10-26 15:05:06 +0800

    trying variables

--- Learning Python/variables and data types.py
+++ Learning Python/variables and data types.py
@@ -4,6 +4,6 @@ name = "Hailey"
 age = "20"
 
 print("There was once a young girl named " + name + ".")
-print("Hailey is 20 years old")
-print("She likes to play videogames")
-print("But being 20 makes her feel miserable")
+print("Hailey is " + age + " years old")
+print(name + " likes to play videogames")
+print("But being " + age + " makes her feel miserable")
