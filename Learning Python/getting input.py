d7bb657b67149a795d7b33351f23153c6fb9c8c6
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-10-28 10:51:58 +0800

    another input

--- Learning Python/getting input.py
+++ Learning Python/getting input.py
@@ -1,2 +1,5 @@
 name = input ("What is your name? \nYour name: ")
-print("What's up," + name + "?")
+print("What's up, " + name + " ?")
+
+age = input("What is your age? \nYour age: ")
+print(name + "'s age is " + age)
