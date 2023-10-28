10df1e2f5e9132dbcd2916867467afdaae5772ea
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-10-28 23:37:39 +0800

    append extend

--- Learning Python/list functions.py
+++ Learning Python/list functions.py
@@ -4,3 +4,11 @@ names = ["Hail", "Kendra", "McDuffie", "Maeve", "Hailey", "HailStorm"]
 print("Normal printing of lists:")
 print(names)
 print(ages)
+
+#to append another list to a list
+names.extend(ages)
+print(names)
+
+#to append something on just 1 list
+ages.append(13)
+print(ages)
