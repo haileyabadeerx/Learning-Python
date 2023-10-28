6dbffc96d5848a7d5c6d5df248e2ceb84e9bb976
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-10-28 11:11:04 +0800

    mad libs little program

---
+++ Learning Python/mad libs.py
@@ -0,0 +1,11 @@
+print("Lyrics prompting program")
+
+color = input("Enter a color: ")
+place = input("Enter a place: ")
+season = input("Enter a season: ")
+feels = input("Enter a feels: ")
+
+print("The shape of your body it's " + color)
+print("Breakable " + place)
+print("It's a cruel " + season )
+print("I " + feels + " you, ain't that the worst thing you've ever heard?")
