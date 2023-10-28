dbc333d81e6cf7144564454f71f10b2561ae8ae1
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-10-28 11:21:02 +0800

    accessing index reversely

--- Learning Python/lists.py
+++ Learning Python/lists.py
@@ -3,4 +3,7 @@ albums = ["Lover", "Reputation", "1989"]
 #pwede rin mixed var types in one like
 #albums = ["Red", 5, True] (string, int, and bool in one list)
 
-print(albums[2])
+print(albums[0])
+
+#to access index from the last element
+print(albums[-1])
