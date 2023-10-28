e0ee51f051b171cb8eb9c5a60ab00d02989c6de4
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-10-29 00:14:08 +0800

    functions

---
+++ functions.py
@@ -0,0 +1,10 @@
+def greetings():
+    print("Welcome to functions in Python")
+    name = input("Your name? ")
+    print("Nice meeting you " + name + ".")
+
+#to call a function
+
+print("This is how function works in Python")
+greetings()
+print("Kewl, isn't it?")
