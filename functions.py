b3998b942e9cf6e1ba83e3961fd5eee491e6d47b
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-10-29 00:21:27 +0800

    multi parameters

--- functions.py
+++ functions.py
@@ -1,9 +1,10 @@
-def greetings(name):
+def greetings(name, days):
     print("Welcome to functions in Python")
-    print("Nice meeting you " + name + ".")
+    print("Nice meeting you " + name + ". \n So you are learning Python for " + str(days) + " days.")
 
 #to call a function with passing parameters
 print("This is how function works in Python")
 nam = input("Your name? ")
-greetings(nam)
+das = input("How long are you learning python?")
+greetings(nam, das)
 print("Kewl, isn't it?")
