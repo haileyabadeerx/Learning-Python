6e050d5e4e3f360ef1af6df070a211c26a8d7bff
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-10-28 10:59:52 +0800

    adding numbers as strings

---
+++ Learning Python/type casting.py
@@ -0,0 +1,8 @@
+#trying to make a simple addition calcu here to show how type casting works
+
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+
+result = num1 + num2
+
+print(result)
