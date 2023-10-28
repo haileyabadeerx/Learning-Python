5196037eee1bcef918c00826679bde1b74a1e0f2
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-10-28 23:49:58 +0800

    sort

--- Learning Python/list functions.py
+++ Learning Python/list functions.py
@@ -1,12 +1,23 @@
-ages = [9, 14, 15, 16, 20, 21]
+ages = [9, 14, 15, 16, 20, 21, 3, 3, 3, 3]
 names = ["Hail", "Kendra", "McDuffie", "Maeve", "Hailey", "HailStorm"]
 
 print("Normal printing of lists:")
 print(names)
 print(ages)
 
+#sort in ascending order/alphabetical
+names.sort()
+ages.sort()
+
+print(ages)
+print(names)
+
 #to search for an index
+#pag nagsearch nang wala sa list mag eerror
 print(names.index("Maeve"))
+
+#count ilang duplications meron ang element
+print(ages.count(3))
 
 #to append another list to a list
 names.extend(ages)
