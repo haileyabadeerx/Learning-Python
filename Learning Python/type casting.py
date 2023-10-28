aa86ad79d0ed6992fe83e0b8fe3de44b29451422
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-10-28 23:25:03 +0800

    ewan

--- Learning Python/type casting.py
+++ Learning Python/type casting.py
@@ -6,7 +6,7 @@ num2 = input("Enter second number: ")
 result = num1 + num2 #di pa naka type casting so inadd sya as strings kaya wrong value
 print(result)
 
-result = float(num1) + float(num2)
+result = float(num1) + float(num2) #tama na to and without errors kasi nakakahandle sya ng decimal nums
 print(result)
 
 result = int(num1) + int(num2) #ito magiging tama na kasi naconvert na to int pero pag naglagay decimal mag eerror
