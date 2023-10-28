a8d71dfccc7165f9b9f67e726793138a5230718a
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-10-28 11:25:41 +0800

    choosing range of lists

--- Learning Python/lists.py
+++ Learning Python/lists.py
@@ -1,4 +1,4 @@
-albums = ["Lover", "Reputation", "1989"]
+albums = ["Lover", "Reputation", "1989", "Red", "Speak Now", "Fearless"]
 
 #pwede rin mixed var types in one like
 #albums = ["Red", 5, True] (string, int, and bool in one list)
@@ -10,3 +10,6 @@ print(albums[-1])
 
 #to exclude certain elements kunwari ayaw ipakita ang 1st element us2 yung the rest 
 print(albums[1:])
+
+#choosing range of elements
+print(albums[1:5])
