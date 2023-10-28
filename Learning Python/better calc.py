c5f8a21d4e295d35eee34f0e2b46444e9faa6943
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-10-29 01:30:35 +0800

    type casting change error

--- Learning Python/better calc.py
+++ Learning Python/better calc.py
@@ -1,17 +1,17 @@
 def addition(a, b):
-    result = int(a) + int(b)
+    result = a + b
     return result
 
 def subtraction(a, b):
-    result = int(a) - int(b)
+    result = a - b
     return result
 
 def multiplication(a, b):
-    result = int(a) * int(b)
+    result = a * b
     return result
 
 def division(a, b):
-    result = int(a) / int(b)
+    result = a / b
     return result
 
 print("This program can calculate basic operations.")
@@ -25,13 +25,13 @@ print("Please input the operation you wish to perform: ")
 op = input()
 
 if op == "+":
-    answer = addition(num1, num2)
+    answer = int(addition(num1, num2))
 elif op == "-":
-    answer = subtraction(num1, num2)
+    answer = int(subtraction(num1, num2))
 elif op == "*":
-    answer = multiplication(num1, num2)
+    answer = int(multiplication(num1, num2))
 elif op == "/":
-    answer = division(num1, num2)
+    answer = int(division(num1, num2))
 else:
     print("Invalid input.")
     is_invalid = True
@@ -39,4 +39,4 @@ else:
 if is_invalid:
     print("Try again.")
 else:
-    print("The answer is " + str(answer))
+    print("The answer is " + answer)
