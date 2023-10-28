ef53a61e085f4ee648f81b135fe9d63ff48dd4e6
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-10-28 23:40:50 +0800

    remove insert

--- Learning Python/list functions.py
+++ Learning Python/list functions.py
@@ -12,3 +12,11 @@ print(names)
 #to append something on just 1 list
 ages.append(13)
 print(ages)
+
+#to insert on specific element
+ages.insert(0, 7)
+print(ages) #once na may ininsert, lahat mapupush
+
+#to remove an element
+names.remove("McDuffie")
+print(names)
