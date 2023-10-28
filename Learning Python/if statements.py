81639e4731d125f44a1da32bd901216aa7113923
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-10-29 01:05:04 +0800

    if statements

--- Learning Python/if statements.py
+++ Learning Python/if statements.py
@@ -1,3 +1,4 @@
+#simple if statements
 is_single = True
 is_broken = True
 
@@ -9,3 +10,19 @@ elif is_single and not(is_broken):
     print("Not broken yet single")
 else:
     print("Woah, neither. Congrats!")
+
+#with comparisons
+
+def smallest(a, b, c):
+    if a <= b and a <= c:
+        return a
+    elif b <= a and b <= c:
+        return b
+    else:
+        return c
+
+print("Enter 3 numbers: ")
+c = input()
+a = input() 
+b = input()
+print("The smallest number is " + smallest(c, a, b))
