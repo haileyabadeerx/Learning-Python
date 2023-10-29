4435fe43a845f8d9dc2b7d610b26ff40f91968fb
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-10-30 01:35:19 +0800

    error fixed

--- Learning Python/dictionaries.py
+++ Learning Python/dictionaries.py
@@ -14,13 +14,11 @@ MidnightsTrack = {
     12: "Sweet Nothing",
     13: "Mastermind",
 }
-num = 4
-print(MidnightsTrack[num])
 
 print("This is a Midnights Album Tracker")
 print("Press a number from 1-13 for your track.")
 
-track = input("Enter here: ")
+track = int(input("Enter here: "))
 
 if track in MidnightsTrack:
     print(MidnightsTrack[track])
