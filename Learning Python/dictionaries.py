d487f5de018cc88ff9cc021617acefed48fad409
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-10-30 01:28:13 +0800

    input tracks

--- Learning Python/dictionaries.py
+++ Learning Python/dictionaries.py
@@ -14,3 +14,8 @@ MidnightsTrack = {
     12: "Sweet Nothing",
     13: "Mastermind",
 }
+
+print("This is a Midnights Album Tracker")
+print("Press a number from 1-13 for your track.")
+
+track = input("Enter here: ")
