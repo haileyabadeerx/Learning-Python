c6c3bdbe460978bacda7bf5f57d6f0ceac070773
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-10-31 01:34:41 +0800

    error again

--- Learning Python/practice - guess the word.py
+++ Learning Python/practice - guess the word.py
@@ -11,10 +11,11 @@ guess_lower = ""
 guess_limit = 3
 guess_count = 0
 
-while le_word.lower() != guess_lower:
+while le_word.lower() != guess_lower and guess_count < 3:
     print("Oops, try again.")
     print(str(guess_limit - 1) + "attempts remaining.")
     guess_limit -= 1
+    guess_count += 1
     ask_input()
 
 print("Congratulations! You know my favorite character.")
