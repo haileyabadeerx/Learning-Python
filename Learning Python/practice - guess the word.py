4b9fcf1db31b31824b47c1f7694619462e7de1aa
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-10-31 01:49:58 +0800

    guess count removed

--- Learning Python/practice - guess the word.py
+++ Learning Python/practice - guess the word.py
@@ -2,20 +2,17 @@ le_word = "Hu Tao"
 
 def ask_input():
     guess = input("Enter your guess: ")
-    guess_lower = guess.lower()
 
 ask_input()
 
 guess = ""
-guess_lower = ""
 guess_limit = 3
 guess_count = 0
 
-while guess_lower != le_word.lower() and guess_count < 3 and guess_limit != 0 :
+while guess.lower() != le_word.lower() and guess_limit != 0 :
     print("Oops, try again.")
     print(str(guess_limit) + " attempts remaining.")
     guess_limit -= 1
-    guess_count += 1
     got_it = False
     ask_input()
     
