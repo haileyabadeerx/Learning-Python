cb9521e9a172d745162dc00103cf2ee2273ccab9
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-10-31 01:36:51 +0800

    guess count fixed

--- Learning Python/practice - guess the word.py
+++ Learning Python/practice - guess the word.py
@@ -13,7 +13,7 @@ guess_count = 0
 
 while le_word.lower() != guess_lower and guess_count < 3:
     print("Oops, try again.")
-    print(str(guess_limit - 1) + "attempts remaining.")
+    print(str(guess_limit) + " attempts remaining.")
     guess_limit -= 1
     guess_count += 1
     ask_input()
