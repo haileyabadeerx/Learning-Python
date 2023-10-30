b5d2ee0c29c150183478611cafaed9785ba19c3f
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-10-31 01:33:01 +0800

    error

--- Learning Python/practice - guess the word.py
+++ Learning Python/practice - guess the word.py
@@ -4,12 +4,14 @@ def ask_input():
     guess = input("Enter your guess: ")
     guess_lower = guess.lower()
 
+ask_input()
+
 guess = ""
 guess_lower = ""
 guess_limit = 3
 guess_count = 0
 
-while guess != guess_lower:
+while le_word.lower() != guess_lower:
     print("Oops, try again.")
     print(str(guess_limit - 1) + "attempts remaining.")
     guess_limit -= 1
