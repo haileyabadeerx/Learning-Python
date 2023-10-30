3d0e64948776bc8df114574294f73ffae4bdeb24
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-10-31 01:18:26 +0800

    guess game 2nd commit

--- Learning Python/practice - guess the word.py
+++ Learning Python/practice - guess the word.py
@@ -1,6 +1,13 @@
 le_word = "Hu Tao"
 guess = ""
+guess_limit = 3
+guess_count = 0
 
 while guess != le_word:
     guess = input.lowercase("Enter your guess: ")
-    
+    if guess != le_word:
+        print("Oops, try again.")
+        print(guess_limit - 1 + "attempts remaining.")
+
+
+print("Congratulations! You know my favorite character.")
