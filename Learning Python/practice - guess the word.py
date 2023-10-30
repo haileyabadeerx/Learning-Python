185c729c809fd3e1213a4fe017ddc7023d2dbd6a
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-10-31 01:31:19 +0800

    error:

--- Learning Python/practice - guess the word.py
+++ Learning Python/practice - guess the word.py
@@ -1,16 +1,18 @@
 le_word = "Hu Tao"
+
+def ask_input():
+    guess = input("Enter your guess: ")
+    guess_lower = guess.lower()
+
 guess = ""
 guess_lower = ""
 guess_limit = 3
 guess_count = 0
 
 while guess != guess_lower:
-    guess = input("Enter your guess: ")
-    guess_lower = guess.lower()
-    if guess != guess_lower:
-        print("Oops, try again.")
-        print(str(guess_limit - 1) + "attempts remaining.")
-        guess_limit -= 1
-
+    print("Oops, try again.")
+    print(str(guess_limit - 1) + "attempts remaining.")
+    guess_limit -= 1
+    ask_input()
 
 print("Congratulations! You know my favorite character.")
