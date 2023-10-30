47ceb29f576bf4ee3489d1e14fa18ee1bbe96191
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-10-31 03:17:51 +0800

    practice guess the word program completed 3:17 am

--- Learning Python/practice - guess the word.py
+++ Learning Python/practice - guess the word.py
@@ -1,23 +1,20 @@
 le_word = "Hu Tao"
 
-def ask_input():
-    guess = input("Enter your guess: ")
-
 guess_limit = 3
-got_it = False
 
-while guess_limit != 0:
+print("Can you guess my FAVORITE GENSHIN IMPACT CHARACTER?")
+
+while guess_limit >= 0:
     guess = input("Enter your guess: ")
 
-    if guess.lower() != le_word.lower():
+    if guess.lower() != le_word.lower() and guess_limit != 0:
         print("Oops, try again.")
         print(str(guess_limit) + " attempts remaining.")
         guess_limit -= 1
-        ask_input()
     else:
         break
     
-if got_it:
-    print("Sorry, you ran out of attempts.")
+if guess_limit == 0 and guess.lower() != le_word.lower():
+    print("Sorry, you ran out of attempts.")   
 else:
     print("Congratulations! You know my favorite character.")
