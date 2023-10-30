54bdd3be53664ae09046e86c4241e2b79bddd29a
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-10-31 01:42:56 +0800

    try fix

--- Learning Python/practice - guess the word.py
+++ Learning Python/practice - guess the word.py
@@ -11,11 +11,15 @@ guess_lower = ""
 guess_limit = 3
 guess_count = 0
 
-while le_word.lower() != guess_lower or guess_count < 3:
+while le_word.lower() != guess_lower and guess_count < 3 and guess_limit != 0 :
     print("Oops, try again.")
     print(str(guess_limit) + " attempts remaining.")
     guess_limit -= 1
     guess_count += 1
+    got_it = False
     ask_input()
-
-print("Congratulations! You know my favorite character.")
+    
+if got_it = False:
+    print("Sorry, you ran out of attempts.")
+else:
+    print("Congratulations! You know my favorite character.")
