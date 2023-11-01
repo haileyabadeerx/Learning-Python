e1d9e403a2494eb93831e6b96ec5e38d5934e60e
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-11-01 20:15:57 +0800

    error

---
+++ Learning Python/build a translator.py
@@ -0,0 +1,11 @@
+def translate(phrase):
+    translation = ""
+
+    for letter in phrase:
+        if letter in "AEIOU"
+            translation = translation + "meow"
+        else:
+            translation = translation + letter
+    return translation
+
+print(translate(input("Enter something to translate: ")))
