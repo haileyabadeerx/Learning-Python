a81a2d5a576745e3cd3b7613c3a3d0211e65dd40
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-11-01 19:31:09 +0800

    for loop arrays

--- Learning Python/for loops.py
+++ Learning Python/for loops.py
@@ -1,3 +1,11 @@
 #this is how to declare basic for loop
+#dito niloloop lang letters nung string dito
 for  letter in "Honkai Star Rail":
     print(letter)
+
+#for arrays
+
+trailblazers = ["March 7th", "Dan Heng", "Stelle"]
+
+for member in trailblazers:
+    print(member)
