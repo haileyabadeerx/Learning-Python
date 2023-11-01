1b0f7bf215f2e6f710882bf563ae1c0db674cae3
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-11-01 20:07:47 +0800

    nested loops

--- Learning Python/2D lists & Nested Loops.py
+++ Learning Python/2D lists & Nested Loops.py
@@ -9,3 +9,8 @@ row = int(input("What row you want to see?"))
 column = int(input("What column you want to see?"))
 
 print(twodimension_list[row][column])
+
+#can use nested for loop for printing 2d list
+for row in twodimension_list:
+    for column in row:
+        print(column)
