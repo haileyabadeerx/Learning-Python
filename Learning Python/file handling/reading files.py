60b41e5920abdf09de0e724bf4a297cab8cc125f
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-11-02 14:07:07 +0800

    read fixed

--- Learning Python/file handling/reading files.py
+++ Learning Python/file handling/reading files.py
@@ -3,8 +3,12 @@
 fontaine = open(r"D:\github\Learning Python\Learning Python\file handling\fontaine_charas.txt", "r")
 #may r sa unahan para maread as raw string since path lang nareread ko hindi yung file lang
 print(fontaine.readable()) #<-- check if the file is readable
-print(fontaine.read()) #<-- read everything in the file
 print(fontaine.readline()) #<-- just reads an indvidual line (by default, yung una lang mareread nya)
+print(fontaine.readline())
+print(fontaine.readline())
+
+print(fontaine.read()) #<-- read everything in the file
+
 
 
 fontaine.close()
