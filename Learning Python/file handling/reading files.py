5c04b689b52f2fa6d602cada2b4e06f795b2c2c6
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-11-02 14:01:06 +0800

    solved

--- Learning Python/file handling/reading files.py
+++ Learning Python/file handling/reading files.py
@@ -1,7 +1,7 @@
 #similar to file handling lesson in c
 
 fontaine = open(r"D:\github\Learning Python\Learning Python\file handling\fontaine_charas.txt", "r")
-
+#may r sa unahan para maread as raw string since path lang nareread ko hindi yung file lang
 print(fontaine.read())
 
 fontaine.close()
