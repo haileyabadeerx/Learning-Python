f032bc8228bf585a8aff88b272facfb8c5a5deb7
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-11-02 13:44:33 +0800

    trying to fix

--- Learning Python/file handling/reading files.py
+++ Learning Python/file handling/reading files.py
@@ -1,7 +1,23 @@
-#similar to file handling lesson in c
+import os
 
-fontaine = open("Learning Python\fontaine_charas.txt", "r")
+# Get the absolute path of the current script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Specify the file path relative to the script's location
+file_path = os.path.join(script_dir, "fontaine_charas.txt")
+
+# Open the file
+with open(file_path, 'r') as file:
+    content = file.read()
+    print(content)
+
+
+
+#similar to file handling lesson in c
+'''
+fontaine = open("fontaine_charas.txt", "r")
 
 print(fontaine.readable())
 
 fontaine.close()
+'''
