cc6059231b3979d5dda4dcf68add5eb6dae796ee
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-11-04 02:19:34 +0800

    class chara

--- Learning Python/classes and objects/characters.py
+++ Learning Python/classes and objects/characters.py
@@ -1,2 +1,6 @@
 class chara:
-    
+    def __init__(self, name, path, money, is_dead)
+        self.name = name
+        self.path = path
+        self.money = money
+        self.is_dead = is_dead
