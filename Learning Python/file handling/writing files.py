80ea7947a3b85c8cd85782377e559e19431d77bb
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-11-03 12:17:08 +0800

    try appending

--- Learning Python/file handling/writing files.py
+++ Learning Python/file handling/writing files.py
@@ -1,3 +1,7 @@
 fontaine = open(r"D:\github\Learning Python\Learning Python\file handling\fontaine_charas.txt", "a")
 
-fontaine = input(fontaine.write("Charlotte"))
+add_char = input("Enter a fontaine character: ")
+
+fontaine.write(add_char)
+
+fontaine.close()
