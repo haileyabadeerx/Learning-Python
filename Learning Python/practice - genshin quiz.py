a0ca15407c0a0fd1cb5a23ad735d87fc16704e38
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-11-05 22:32:06 +0800

    question prompts done

--- Learning Python/practice - genshin quiz.py
+++ Learning Python/practice - genshin quiz.py
@@ -3,6 +3,7 @@ import quiz_ques from questions:
 q_prompts = [
     "What is Venti's true identity? \n(a) Drunk Man  (b) Wind Spirit  (c) Klee's Mum\n\nAnswer: ",
     "Where do Zhongli works as a human now? \n(a) Wangsheng Funeral Parlor  (b) Cat Tail's Bar  (c) Port Ormos\n\nAnswer: ",
-    "Can Raiden Shogun cook? \n(a) Yes  (b) Who knows  (c) No\n\nAnswer: "
-    
+    "Can Raiden Shogun cook? \n(a) Yes  (b) Who knows  (c) No\n\nAnswer: ",
+    "Nahida's archon name based on a demon is ____. \n(a) Baal  (b) Kusanali  (c) Buer\n\nAnswer: ",
+    "Who tried to kill Furina? \n(a) Neuvilette  (b) Arlecchino  (c) Traveler\n\nAnswer: "
 ]
