fd69e7c207183a10d29a197123010358d2020d0d
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-11-05 22:28:30 +0800

    initial questions done

---
+++ Learning Python/practice - genshin quiz.py
@@ -0,0 +1,8 @@
+import quiz_ques from questions:
+
+q_prompts = [
+    "What is Venti's true identity? \n(a) Drunk Man  (b) Wind Spirit  (c) Klee's Mum\n\nAnswer: ",
+    "Where do Zhongli works as a human now? \n(a) Wangsheng Funeral Parlor  (b) Cat Tail's Bar  (c) Port Ormos\n\nAnswer: ",
+    "Can Raiden Shogun cook? \n(a) Yes  (b) Who knows  (c) No\n\nAnswer: "
+    
+]
