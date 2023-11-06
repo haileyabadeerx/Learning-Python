086a8ac6637c2864a6d042788a076c84b7fc9a69
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-11-06 15:41:36 +0800

    gi chara function declared

--- Learning Python/classes and objects/characters.py
+++ Learning Python/classes and objects/characters.py
@@ -4,3 +4,20 @@ class hsr_chara:
         self.path = path
         self.age = age
         self.is_dead = is_dead
+
+class gi_chara:
+    def __init__(self, name, element, weapon, age):
+        self.name = name
+        self.element = element
+        self.weapon = weapon
+        self.age = age
+
+    def lage_range(self):
+        if gi_chara.age <= 19 and gi_chara > 12:
+            return print("This character is a teenager.")
+        elif gi_chara.age <= 12:
+            return print("This character is a child.")
+        elif gi_chara.age > 19:
+            return print("This character is an adult.")
+        else:
+            return print("Invalid inputted age.")
