d40c4eeaa8771b3580fc5fa553dd0c917617442a
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-11-06 15:53:51 +0800

    converted age var to int

--- Learning Python/classes and objects/object funcs.py
+++ Learning Python/classes and objects/object funcs.py
@@ -3,7 +3,7 @@ from characters import gi_chara
 name = input("Character name: ")
 element = input("Character's element: ")
 weapon = input("Character's weapon: ")
-age = input("Character's age: ")
+age = int(input("Character's age: "))
 
 char = gi_chara(name, element, weapon, age)
 
