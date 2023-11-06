df4dacc68f011c9d5213fe6548ddfcaea3e85f40
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-11-06 15:44:34 +0800

    error occured

---
+++ Learning Python/classes and objects/object funcs.py
@@ -0,0 +1,13 @@
+from characters import gi_chara
+
+name = input("Character name: ")
+element = input("Character's element: ")
+weapon = input("Character's weapon: ")
+age = input("Character's age: ")
+
+char = gi_chara(name, path, age, True)
+
+print(char.name)
+print(char.element)
+print(char.weapon)
+print(char.age)
