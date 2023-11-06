b88fdae85e6d780f4c0587fb1e8d6375f111474d
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-11-07 01:57:24 +0800

    problem fixed

---
+++ Learning Python/inheritance/animegames.py
@@ -0,0 +1,12 @@
+from hoyogames import hoyogames
+
+class animegames(hoyogames):
+
+    def SAOMD(self):
+        print("Sword Art Online: Memory Defrag")
+
+    def AL(self):
+        print("Azur Lane")
+    
+    def PG(self):
+        print("Pokemon GO")
