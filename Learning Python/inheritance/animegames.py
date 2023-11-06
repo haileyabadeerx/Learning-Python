a4dd3655f3b833a23f49b99d61705579c428bd55
haileyabadeerx <haileykendramcduffie@gmail.com>
2023-11-07 01:59:21 +0800

    overwriting method

--- Learning Python/inheritance/animegames.py
+++ Learning Python/inheritance/animegames.py
@@ -2,6 +2,9 @@ from hoyogames import hoyogames
 
 class animegames(hoyogames):
 
+    def GenshinImpact(self):
+        print("You can overwrite it too using this inheritance method.")
+
     def SAOMD(self):
         print("Sword Art Online: Memory Defrag")
 
